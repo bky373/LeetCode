3da7af9729393cd207fbcfb9bc908b4f116a36c8
Borahm <bo373@naver.com>
2024-02-21 22:15:30 +0900

    Time: 4 ms (100.00%), Space: 43.7 MB (90.45%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,15 +1,19 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if (x < 0) {
+        // Special cases: Negative numbers and numbers ending with 0 are not palindromes
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
             return false;
         }
-        int temp = x;
+        
         int reversed = 0;
-
-        while (temp != 0) {
-            reversed = reversed * 10 + temp % 10;
-            temp /= 10;
+        while (x > reversed) {
+            // Reverse the number by adding the last digit to reversed
+            reversed = reversed * 10 + x % 10;
+            // Move to the next digit in x
+            x /= 10;
         }
-        return reversed == x;
+        
+        // If the length of x is odd, we can ignore the middle digit
+        return x == reversed || x == reversed / 10;
     }
 }
