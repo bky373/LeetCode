d6d7030f4dd5459f0e1b05b4453818eed198bf84
Borahm <bo373@naver.com>
2024-04-03 17:52:49 +0900

    Time: 0 ms (100.00%), Space: 42 MB (9.00%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,26 +1,15 @@
 class Solution {
     public int[] plusOne(int[] digits) {
         int n = digits.length;
-        int[] answer = new int[n + 1];
-        int incr = 0;
-        for (int i=n-1;i>=0; i--) {
-            int tmp;
-            if (i == n-1) {
-                tmp = digits[i] + 1;
-            } else {
-                tmp = digits[i] + incr;
+        for (int i=n-1; i>=0; --i) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
             }
-            incr = 0;
-            if (tmp > 9) {
-                incr++;
-            }
-            answer[i] = tmp % 10;
+            digits[i] = 0;
         }
-        if (incr == 1) {
-            answer[0] = 1;
-            return Arrays.copyOfRange(answer, 0, n+1);
-        } else {
-            return Arrays.copyOfRange(answer, 0, n);
-        }
+        digits = new int[n+1];
+        digits[0]++;
+        return digits;
     }
 }
