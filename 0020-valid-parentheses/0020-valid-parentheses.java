2b2b7f5890c731a7d0745fc00ff2621382459220
Borahm <bo373@naver.com>
2024-04-25 19:01:10 +0900

    Time: 3 ms (11.62%), Space: 41.7 MB (19.25%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        Map<Character, Character> pairs = Map.of('(', ')', '{', '}', '[', ']');
+        
+        for (int i=0; i<s.length(); i++) {
+            if (pairs.containsKey(s.charAt(i))) {
+                st.push(s.charAt(i));
+            } else if (st.isEmpty()) {
+                return false;
+            } else {
+                if (s.charAt(i) != pairs.get(st.pop())) {
+                    return false;
+                }
+            }
+        }
+        if (st.isEmpty()) {
+            return true;
+        }
+        return false;
+    }
+}
