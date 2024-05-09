d52756c28056ba6884b88aa20957db48e7fe1bdd
Borahm <bo373@naver.com>
2024-05-09 22:24:10 +0900

    Time: 10 ms (10.02%), Space: 40.2 MB (57.89%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,15 +1,11 @@
 class Solution {
     public int fib(int n) {
-        return f(n);
-    }
-    
-    public int f(int n) {
         if (n <= 0) {
             return 0;
         }
         if (n == 1) {
             return 1;
         }
-        return f(n-1) + f(n-2);
+        return fib(n-1) + fib(n-2);
     }
 }
