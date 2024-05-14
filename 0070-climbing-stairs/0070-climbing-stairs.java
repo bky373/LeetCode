8c11f974e71809887279b0564d37bba1b9d751c9
Borahm <bo373@naver.com>
2024-05-15 08:19:24 +0900

    Time: 0 ms (100.00%), Space: 40 MB (73.55%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,20 +1,17 @@
 class Solution {
     public int climbStairs(int n) {
-        int[] memo = new int[n+1];
-        return checkSteps(0, n, memo);
-    }
-    
-    public int checkSteps(int i, int n, int[] memo) {
-        if (i > n) {
-            return 0;
+        if (n==1 || n==2) {
+            return n;
         }
-        if (i == n) {
-            return 1;
+        int stepsBy1 = 1;
+        int stepsBy2 = 2;
+        int cur = stepsBy1 + stepsBy2;
+        
+        for (int i=4; i<n+1; i++) {
+            stepsBy1 = stepsBy2;
+            stepsBy2 = cur;
+            cur = stepsBy1 + stepsBy2;
         }
-        if (memo[i] > 0) {
-            return memo[i];
-        }
-        memo[i] = checkSteps(i+1, n, memo) + checkSteps(i+2, n, memo);
-        return memo[i];
+        return cur;
     }
 }
