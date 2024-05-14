6eb9ad82eaa58e373cb57ee9a6fb73e39e3b42fd
Borahm <bo373@naver.com>
2024-05-15 08:20:30 +0900

    Time: 0 ms (100.00%), Space: 40.2 MB (45.57%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,17 +1,16 @@
 class Solution {
     public int climbStairs(int n) {
-        if (n==1 || n==2) {
+        if (n==1) {
             return n;
         }
         int stepsBy1 = 1;
         int stepsBy2 = 2;
-        int cur = stepsBy1 + stepsBy2;
         
-        for (int i=4; i<n+1; i++) {
+        for (int i=3; i<n+1; i++) {
+            int cur = stepsBy1 + stepsBy2;
             stepsBy1 = stepsBy2;
             stepsBy2 = cur;
-            cur = stepsBy1 + stepsBy2;
         }
-        return cur;
+        return stepsBy2;
     }
 }
