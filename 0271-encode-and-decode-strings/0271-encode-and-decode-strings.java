93bc6c0d1bd3e2531a54798eeac67d802c9d399a
Borahm <bo373@naver.com>
2024-06-06 17:15:04 +0900

    Time: 4 ms (95.05%), Space: 45.4 MB (60.92%) - LeetHub

--- 0271-encode-and-decode-strings/0271-encode-and-decode-strings.java
+++ 0271-encode-and-decode-strings/0271-encode-and-decode-strings.java
@@ -1,28 +1,27 @@
 public class Codec {
-    private static final String EMPTY = ">EmPty<";
-    private static final String DELIMITER = ">>,<<";
 
     // Encodes a list of strings to a single string.
     public String encode(List<String> strs) {
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < strs.size(); i++) {
-            String s = strs.get(i);
-            if (s.isEmpty()) {
-                s = EMPTY;
-            }
-            sb.append(s);
-            if (i != strs.size() - 1) {
-                sb.append(DELIMITER);
-            }
+        for (String str : strs) {
+            sb.append(str.length())
+              .append(':')
+              .append(str);
         }
         return sb.toString();
     }
 
     // Decodes a single string to a list of strings.
     public List<String> decode(String s) {
-        return Arrays.stream(s.split(DELIMITER))
-                     .map(str -> str.replaceAll(EMPTY, ""))
-                     .toList();
+        List<String> decoded = new ArrayList<>();
+            int i = 0;
+            while (i < s.length()) {
+                int searchIndex = s.indexOf(':', i);
+                int chunkSize = Integer.parseInt(s.substring(i, searchIndex));
+                i = searchIndex + chunkSize + 1;
+                decoded.add(s.substring(searchIndex + 1, i));
+            }
+            return decoded;
     }
 }
 
