4f83372caa4eb851e8ee99d36d26c52f870ed8c3
Borahm <bo373@naver.com>
2024-06-06 17:15:11 +0900

    Time: 3 ms (98.67%), Space: 45 MB (94.08%) - LeetHub

--- 0271-encode-and-decode-strings/0271-encode-and-decode-strings.java
+++ 0271-encode-and-decode-strings/0271-encode-and-decode-strings.java
@@ -14,14 +14,14 @@ public class Codec {
     // Decodes a single string to a list of strings.
     public List<String> decode(String s) {
         List<String> decoded = new ArrayList<>();
-            int i = 0;
-            while (i < s.length()) {
-                int searchIndex = s.indexOf(':', i);
-                int chunkSize = Integer.parseInt(s.substring(i, searchIndex));
-                i = searchIndex + chunkSize + 1;
-                decoded.add(s.substring(searchIndex + 1, i));
-            }
-            return decoded;
+        int i = 0;
+        while (i < s.length()) {
+            int searchIndex = s.indexOf(':', i);
+            int chunkSize = Integer.parseInt(s.substring(i, searchIndex));
+            i = searchIndex + chunkSize + 1;
+            decoded.add(s.substring(searchIndex + 1, i));
+        }
+        return decoded;
     }
 }
 
