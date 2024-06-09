181ae1dcaff731df8ee023bf98e2aa0826444dd6
Borahm <bo373@naver.com>
2024-06-09 14:45:22 +0900

    Time: 0 ms (100.00%), Space: 41.4 MB (98.15%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,40 +1,25 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int res = -1;
-        if (nums.length == 1 && nums[0] == target) {
-            return 0;
-        }
-        int minIndex = findMinIndex(nums);
-        int leftResult = binarySearch(nums, 0, minIndex -1, target);
-        if (leftResult > -1) {
-            return leftResult;
-        }
-        return binarySearch(nums, minIndex, nums.length - 1, target);
-    }
-    
-    private int findMinIndex(int[] nums) {
-        int left = 0;
-        int right = nums.length - 1;
-        while (nums[left] > nums[right]) {
-            int mid = (left+right) / 2;
-            if (nums[mid] < nums[right]) {
-                right = mid;
-            } else {
-                left = mid + 1;
-            }
-        }
-        return left;
-    }
-    
-    private int binarySearch(int[] nums, int left, int right, int target) {
-        while (left <= right) {
-            int mid = (left+right) / 2;
+        int left = 0, right = nums.length - 1;
+        while(left <= right) {
+            int mid = left + (right - left) / 2;
+
             if (nums[mid] == target) {
                 return mid;
-            } else if (nums[mid] < target) {
-                left = mid + 1;
+            }
+
+            if(nums[mid] < nums[right]) {
+                if (target < nums[mid] || target > nums[right]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
             } else {
-                right = mid - 1;
+                if (target < nums[left] || target > nums[mid]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
             }
         }
         return -1;
