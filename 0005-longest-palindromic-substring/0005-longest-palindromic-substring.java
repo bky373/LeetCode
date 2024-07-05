f929b271830fbdae036c21dda1da17cadb884585
Borahm <bo373@naver.com>
2024-07-06 00:49:34 +0900

    Time: 15 ms (88.10%), Space: 42 MB (81.81%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -4,22 +4,21 @@ class Solution {
     
     public String longestPalindrome(String s) {
         for (int i = 0; i < s.length(); i++) {
-            calculateMaxLength(i, i, maxStart, maxEnd, s);
-            calculateMaxLength(i, i+1, maxStart, maxEnd, s);
+            calculateMaxLength(i, i, s);
+            calculateMaxLength(i, i+1, s);
         }
         return s.substring(maxStart, maxEnd + 1);
     }
     
-    public void calculateMaxLength(int start, int end, int maxStart, int maxEnd, String s){
+        public void calculateMaxLength(int start, int end, String s){
         while (start >= 0 && end < s.length() && s.charAt(start) == s.charAt(end)) {
-            if (maxEnd - maxStart < end - start) {
-                maxStart = start;
-                maxEnd = end;
+            if (this.maxEnd - this.maxStart < end - start) {
+                this.maxStart = start;
+                this.maxEnd = end;
             }
             start--;
             end++;
         }
-        this.maxStart = maxStart;
-        this.maxEnd = maxEnd;
     }
+
 }
