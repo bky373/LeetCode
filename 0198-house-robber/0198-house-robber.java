d999bc949fa2bc4352db4f0430ac1b60b12ca329
Borahm <bo373@naver.com>
2024-07-06 21:06:45 +0900

    Time: 0 ms (100.00%), Space: 40.7 MB (62.45%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int rob(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        
+        int maxOfTwoStepsAhead = 0;
+        int maxOfOneStepAhead = nums[nums.length-1];
+        
+        for (int i = nums.length-2; i>=0; --i) {
+            int curr = Math.max(maxOfOneStepAhead, maxOfTwoStepsAhead + nums[i]);
+            
+            maxOfTwoStepsAhead = maxOfOneStepAhead;
+            maxOfOneStepAhead = curr;
+        }
+        return maxOfOneStepAhead;
+    }
+}
