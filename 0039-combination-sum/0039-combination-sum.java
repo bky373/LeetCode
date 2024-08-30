4f4ee4c0ae3edeb7c3486327b167b969513af8fa
Borahm Lee <bo373@naver.com>
2024-08-30 21:13:53 +0900

    Time: 2 ms (83.32%), Space: 44.1 MB (97.05%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,26 +1,27 @@
 class Solution {
-    Set<List<Integer>> result = new HashSet<>();
     
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        backtrack(candidates, new ArrayList<Integer>(), 0, target);
-        return result.stream().toList();
+        List<List<Integer>> results = new ArrayList<>();
+        LinkedList<Integer> combination = new LinkedList<>();
+        int startingIndex = 0;
+        
+        this.backtrack(candidates, new ArrayList<Integer>(), startingIndex, target, results);
+        return results;
     }
     
-    void backtrack(int[] candidates, List<Integer> combination, int sum, int target) {
-        if (sum > target) {
+    protected void backtrack(int[] candidates, List<Integer> comb, int start, int remain, List<List<Integer>> results) {
+        if (remain < 0) {
             return;
         }
-        if (sum == target) {
-            List<Integer> comb =  new ArrayList<>(combination);
-            comb.sort(Comparator.naturalOrder());
-            result.add(comb);
+        if (remain == 0) {
+            results.add(new ArrayList<>(comb));
             return;
         }
         
-        for (int i=0; i<candidates.length; i++) {
-            combination.add(candidates[i]);
-            backtrack(candidates, combination, sum + candidates[i], target);
-            combination.remove(combination.size() - 1);            
+        for (int i=start; i<candidates.length; i++) {
+            comb.add(candidates[i]);
+            backtrack(candidates, comb, i, remain - candidates[i], results);
+            comb.removeLast(); 
         }
     }
 }
