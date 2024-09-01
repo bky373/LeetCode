c35662a132a1109743d120529c7c184bbf329275
bky373 <bo373@naver.com>
2024-09-01 20:56:18 +0900

    Add solution of [322. Coin Change]

---
+++ 0322-coin-change/Solution.java
@@ -0,0 +1,23 @@
+class Solution {
+
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount + 1);
+
+        Arrays.fill(dp, max);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            for (int coin : coins) {
+                if (i - coin < 0) {
+                    continue;
+                }
+                dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+            }
+        }
+        return dp[amount] == (amount + 1) ? -1 : dp[amount];
+    }
+}
+/**
+ * Time complexity : O(S∗n). On each step the algorithm finds the next F(i) in n iterations, where 1≤i≤S. Therefore in total the iterations are S∗n.
+ * Space complexity : O(S). We use extra space for the memoization table.
+ */
