72160276dc5b1075f495caa3fb0cf875156bb59c
Borahm Lee <bo373@naver.com>
2024-09-01 20:56:45 +0900

    Time: 15 ms (78.14%), Space: 44.3 MB (59.52%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,20 +1,18 @@
 class Solution {
     
     public int coinChange(int[] coins, int amount) {
-        int max = amount + 1;
-        int[] dp = new int[amount + 1];
-        Arrays.fill(dp, max);
+        int[] dp = new int[amount+1];
+        Arrays.fill(dp, amount+1);
+        
         dp[0] = 0;
         for (int i = 1; i <= amount; i++) {
-            for (int j = 0; j < coins.length; j++) {
-                if (coins[j] <= i) {
-                    dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+            for (int coin : coins) {
+                if (i - coin < 0) {
+                    continue;
                 }
+                dp[i] = Math.min(dp[i], dp[i-coin] + 1);
             }
         }
-        if (dp[amount] > amount) {
-            return -1;
-        }
-        return dp[amount];
+        return dp[amount] == (amount+1) ? -1 : dp[amount];
     }
 }
