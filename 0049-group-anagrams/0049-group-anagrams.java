1dbce6edf415a0c1cf6f89a574fc02f0f0edb47c
bky373 <bo373@naver.com>
2024-09-14 16:11:40 +0900

    Update solutions of [0049. Group Anagrams]

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,4 +1,11 @@
-https://leetcode.com/problems/group-anagrams/
+// https://leetcode.com/problems/group-anagrams/
+
+/**
+ * Approach 1: Categorize by Sorted String
+ *
+ * TC: O(n * klogk), where n is the length of strs, and k is the maximum length of a string in strs.
+ * SC: O(n * k), the total elements stored in answer.
+ */
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         Map<String, List<String>> anagramGroups = new HashMap<>();
@@ -11,7 +18,32 @@ class Solution {
         return new ArrayList(anagramGroups.values());
     }
 }
+
 /**
- * TC: O(n * klogk), where n is the length of strs, and k is the maximum length of a string in strs.
+ * Approach 2: Categorize by Count
+ *
+ * TC: O(n * k), where n is the length of strs, and k is the maximum length of a string in strs.
  * SC: O(n * k), the total elements stored in answer.
  */
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> anagramGroups = new HashMap<>();
+        int[] counts = new int[26];
+
+        for (String word : strs) {
+            Arrays.fill(counts, 0);
+            char[] arr = word.toCharArray();
+            for (char c : arr) {
+                counts[c - 'a']++;
+            }
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < 26; i++) {
+                sb.append("#");
+                sb.append(counts[i]);
+            }
+            anagramGroups.computeIfAbsent(sb.toString(), k -> new ArrayList<>())
+                    .add(word);
+        }
+        return new ArrayList(anagramGroups.values());
+    }
+}
