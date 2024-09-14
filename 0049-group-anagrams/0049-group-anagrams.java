4b572f5db304179c432e0637b7ce7489aa7969c9
bky373 <bo373@naver.com>
2024-09-14 15:54:21 +0900

    Update solution of [0049. Group Anagrams]

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,12 +1,17 @@
+https://leetcode.com/problems/group-anagrams/
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        Map<String, List<String>> groups = new HashMap();
-        for (String str : strs) {
-            char[] arr = str.toCharArray();
+        Map<String, List<String>> anagramGroups = new HashMap<>();
+        for (String word : strs) {
+            char[] arr = word.toCharArray();
             Arrays.sort(arr);
-            groups.computeIfAbsent(new String(arr), k -> new ArrayList<>())
-                  .add(str);
+            anagramGroups.computeIfAbsent(new String(arr), k -> new ArrayList<>())
+                    .add(word);
         }
-        return new ArrayList(groups.values());
+        return new ArrayList(anagramGroups.values());
     }
 }
+/**
+ * TC: O(n * klogk), where n is the length of strs, and k is the maximum length of a string in strs.
+ * SC: O(n * k), the total elements stored in answer.
+ */
