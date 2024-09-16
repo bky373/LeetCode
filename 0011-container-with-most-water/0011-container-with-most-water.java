e2aa9a4e6e8ce13d9399afd3366a9b47169375ed
bky373 <bo373@naver.com>
2024-09-16 16:22:42 +0900

    Update solution of [11. Container With Most Water]

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,19 +1,24 @@
+/**
+ * https://leetcode.com/problems/container-with-most-water/
+ *
+ * Two Pointer Approach
+ * TC: O(n), where n is the length of height.
+ * SC: O(1)
+ */
 class Solution {
     public int maxArea(int[] height) {
         int left = 0;
         int right = height.length - 1;
-        int max = 0;
+        int maxArea = 0;
         while (left < right) {
-            int area = (right - left) * Math.min(height[left], height[right]);
-            if (area > max) {
-                max = area;
-            }
-            if (height[left] < height[right]) {
-                left++;
-            } else {
+            int minHeight = Math.min(height[left], height[right]);
+            maxArea = Math.max(maxArea, (right - left) * minHeight);
+            if (height[left] > height[right]) {
                 right--;
+            } else {
+                left++;
             }
         }
-        return max;
+        return maxArea;
     }
 }
