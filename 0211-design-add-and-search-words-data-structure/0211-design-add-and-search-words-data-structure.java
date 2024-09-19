688e344976dfda20a5e8d66916482d5d3ef236ce
bky373 <bo373@naver.com>
2024-09-19 22:21:24 +0900

    Update solutions of [211. Design Add and Search Words Data Structure]

--- 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.java
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.java
@@ -1,3 +1,15 @@
+/**
+ * https://leetcode.com/problems/design-add-and-search-words-data-structure/description/
+ *
+ * Approach 1.
+ * addWord
+ * - TC: O(n), where n is the length of word.
+ * - SC: O(n), where n is the length of word.
+ *
+ * search
+ * - TC: O(n*26^d), where n is the length of word and d is the number of dot(.)
+ * - SC: O(1)
+ */
 public class WordDictionary {
     private Node root;
 
@@ -45,4 +57,68 @@ public class WordDictionary {
         boolean isEndOfWord;
     }
 
+}
+
+
+/**
+ * https://leetcode.com/problems/design-add-and-search-words-data-structure/description/
+ *
+ * Approach 2.
+ * addWord
+ * - TC: O(n), where n is the length of word.
+ * - SC: O(n), where n is the length of word.
+ *
+ * search
+ * - TC: O(n*26^d), where n is the length of word and d is the number of dot(.)
+ * - SC: O(1)
+ */
+public class WordDictionary {
+    private Node root;
+
+    public WordDictionary() {
+        this.root = new Node();
+    }
+
+    public void addWord(String word) {
+        Node curr = root;
+        for (char c : word.toCharArray()) {
+            if (curr.children[c - 'a'] == null) {
+                curr.children[c - 'a'] = new Node();
+            }
+            curr = curr.children[c - 'a'];
+        }
+        curr.isEndOfWord = true;
+    }
+
+    public boolean search(String word) {
+        return search(word, 0, root);
+    }
+
+    private boolean search(String word, int i, Node curr) {
+        if (curr == null) {
+            return false;
+        }
+        if (i == word.length()) {
+            return curr.isEndOfWord;
+        }
+        char c = word.charAt(i);
+        if (c == '.') {
+            for (int j = 0; j < 26; j++) {
+                if (search(word, i + 1, curr.children[j])) {
+                    return true;
+                }
+            }
+            return false;
+        }
+        return search(word, i + 1, curr.children[c - 'a']);
+    }
+
+    private class Node {
+        private Node[] children;
+        private boolean isEndOfWord;
+
+        public Node() {
+            this.children = new Node[26];
+        }
+    }
 }
